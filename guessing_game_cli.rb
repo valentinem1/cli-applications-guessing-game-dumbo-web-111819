0b9c2462f4a9c40d04424b1e576350775b5d11bc
Valentine Maillard <valentinemaillard1@gmail.com>
2019-11-12 19:35:08 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   random_num = rand(6) + 1
-  user_prompt = "Guess a number between 1 and 6."
+  puts "Guess a number between 1 and 6."
   player_guess = gets.chomp
   
     if random_num == player_guess
