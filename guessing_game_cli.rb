577f6e842b2526883c36e7364273a6feac4b703a
Valentine Maillard <valentinemaillard1@gmail.com>
2019-11-12 19:33:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   user_prompt = "Guess a number between 1 and 6."
   player_guess = gets.chomp
   
-    if player_guess == random_num
+    if random_num == player_guess
       puts "You guessed the correct number!"
     elsif
       puts "Sorry! The computer guessed #{random_num}."
